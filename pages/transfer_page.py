08d8df941873578b236b147d1224fd6d31a67ee9
cherukurigreeshma <greeshmacherukuri18430@gmail.com>
2026-09-17 14:16:45 +0530

    Update workflow to use python -m pytest and verify requirements

--- pages/transfer_page.py
+++ pages/transfer_page.py
@@ -1,17 +1,34 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 
 class TransferPage:
     def __init__(self, driver):
         self.driver = driver
-        self.url = "https://parabank.parasoft.com/parabank/transfer.htm"
+        self.wait = WebDriverWait(driver, 15)
+        
+        # Locators
         self.amount_input = (By.ID, "amount")
-        self.transfer_button = (By.XPATH, "//input[@value='Transfer']")
+        self.from_account_select = (By.ID, "fromAccountId")
+        self.to_account_select = (By.ID, "toAccountId")
+        self.transfer_btn = (By.XPATH, "//input[@value='Transfer']")
+        self.result_message = (By.XPATH, "//div[@id='showResult'] | //span[@class='error']")
 
-    def load(self):
-        self.driver.get(self.url)
+    def transfer_funds(self, amount, from_index=0, to_index=0):
+        # Wait for the amount field to be visible
+        self.wait.until(EC.visibility_of_element_located(self.amount_input)).send_keys(str(amount))
+        
+        # Wait until dropdown options are populated (more than 0 options)
+        self.wait.until(lambda d: len(Select(d.find_element(*self.from_account_select)).options) > 0)
+        
+        from_dropdown = Select(self.driver.find_element(*self.from_account_select))
+        to_dropdown = Select(self.driver.find_element(*self.to_account_select))
+        
+        from_dropdown.select_by_index(from_index)
+        to_dropdown.select_by_index(to_index)
+        
+        # Click transfer
+        self.wait.until(EC.element_to_be_clickable(self.transfer_btn)).click()
 
-    def transfer(self, amount):
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.amount_input)).send_keys(amount)
-        self.driver.find_element(*self.transfer_button).click()
+    def get_result_text(self):
+        return self.wait.until(EC.visibility_of_element_located(self.result_message)).text
--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -1,22 +1,50 @@
-import pytest
-from pages.login_page import LoginPage
-from pages.transfer_page import TransferPage
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.mark.parametrize("amount,should_succeed", [
-    ("100", True),
-    ("0", False),
-    ("-50", False),
-])
-def test_transfer_amounts(driver, amount, should_succeed):
-    login_page = LoginPage(driver)
-    login_page.load()
-    login_page.login("john", "demo")
-    
-    transfer_page = TransferPage(driver)
-    transfer_page.load()
-    transfer_page.transfer(amount)
-    
-    if should_succeed:
-        assert "complete" in driver.page_source.lower()
-    else:
-        assert "error" in driver.page_source.lower() or "complete" not in driver.page_source.lower()
+class TransferPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
+        
+        # Locators
+        self.amount_input = (By.ID, "amount")
+        self.from_account_select = (By.ID, "fromAccountId")
+        self.to_account_select = (By.ID, "toAccountId")
+        self.transfer_btn = (By.XPATH, "//input[@value='Transfer']")
+        self.result_container = (By.ID, "showResult")
+        self.error_message = (By.CLASS_NAME, "error")
+
+    def transfer_funds(self, amount, from_index=0, to_index=0):
+        # 1. Wait for amount field to be visible
+        amount_element = self.wait.until(EC.visibility_of_element_located(self.amount_input))
+        amount_element.clear()
+        amount_element.send_keys(str(amount))
+        
+        # 2. Wait until AJAX populates the dropdowns (at least 1 option must exist)
+        self.wait.until(
+            lambda d: len(Select(d.find_element(*self.from_account_select)).options) > 0
+        )
+        
+        # Select options
+        from_dropdown = Select(self.driver.find_element(*self.from_account_select))
+        to_dropdown = Select(self.driver.find_element(*self.to_account_select))
+        
+        from_dropdown.select_by_index(from_index)
+        to_dropdown.select_by_index(to_index)
+        
+        # Brief pause to ensure DOM state stabilizes before form submit
+        time.sleep(1)
+        
+        # 3. Click transfer
+        self.wait.until(EC.element_to_be_clickable(self.transfer_btn)).click()
+
+    def get_result_text(self):
+        # Wait for either result container or error message to appear
+        element = self.wait.until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//div[@id='showResult'] | //span[@class='error'] | //p[@class='error']")
+            )
+        )
+        return element.text
